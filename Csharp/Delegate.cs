3e4d71bdde0cb87cae55072d43432eac5f91787c
cucla <cucla@users.noreply.github.com>
2018-05-06 13:05:31 +0800

    Update Delegate.cs

--- Csharp/Delegate.cs
+++ Csharp/Delegate.cs
@@ -7,8 +7,6 @@ namespace Delegate
 
     class MyClass
     {
-        // Delegates can be bound to instance members as well as
-        // static class functions
         public string instanceMethod1(int arg1, int arg2)
         {
             return ((arg1 + arg2) * arg1).ToString();
