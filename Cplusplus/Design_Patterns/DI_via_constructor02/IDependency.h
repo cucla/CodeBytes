3a8386fc734e10a4f5f7ca4b79f78710d10aa954
cucla <cucla@users.noreply.github.com>
2018-05-31 15:51:32 +0800

    Update IDependency.h

--- Cplusplus/Design_Patterns/DI_via_constructor02/IDependency.h
+++ Cplusplus/Design_Patterns/DI_via_constructor02/IDependency.h
@@ -7,16 +7,18 @@ public:
 	virtual void printit() = 0;
 };
 
-/* NOTE:	- naming convevtion make- get- use-
+/* NOTE:  - naming convevtion make- get- use-
 *
 * std::unique_ptr<IFoo> makeFoo();	- passes ownership to the caller;
-*									  unique_ptr is later moved to the object, which ends up owning it
+*					  unique_ptr is later moved to the object, which ends up owning it
 * std::shared_ptr<IFoo> getFoo();	- get an object that other objects might hold a reference to
-* IFoo& useFoo();					- if there already exists an IFoo object which is owned by someone else,
-*									  with the guarantee that it will outlive the caller
+* IFoo& useFoo();			- if there already exists an IFoo object which is owned by someone else,
+*					  with the guarantee that it will outlive the caller
 */
+
 std::unique_ptr<IDependency> makeImpl1();
 std::unique_ptr<IDependency> makeImpl2();
 
 std::shared_ptr<IDependency> getImpl1();
 std::shared_ptr<IDependency> getImpl2();
+
