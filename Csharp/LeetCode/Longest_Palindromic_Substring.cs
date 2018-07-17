a4cbbd3ff656cd64f75cb14e0e2ad7d1e2a18c2d
cucla <cucla@users.noreply.github.com>
2018-07-17 11:00:25 +0800

    Update Longest_Palindromic_Substring.cs

--- Csharp/LeetCode/Longest_Palindromic_Substring.cs
+++ Csharp/LeetCode/Longest_Palindromic_Substring.cs
@@ -1,6 +1,7 @@
+// PROBLEMS: 5, 
+
 // 5. Longest Palindromic Substring
 // Dynamic programming, O(N^2) time and space complexity
-
     public class Solution
     {
         public string LongestPalindrome(string s)
@@ -24,5 +25,59 @@
                 }
             }
             return res;
+        }
+    }
+
+// Manacher’s algorithm, O(N) time
+// EXPLANATION: http://www.zrzahid.com/longest-palindromic-substring-in-linear-time/
+    public class Solution
+    {
+        public string LongestPalindrome(string s)
+        {
+            var res = string.Empty;
+            var reslen = 0;
+
+            var swithpounds = preprocess(s);
+            var maxR = 0;
+            var maxC = 0;
+            var dp = new int[swithpounds.Length];
+            for (int i = 0; i < swithpounds.Length; i++)
+            {
+                dp[i] = maxR > i ? Math.Min(dp[2 * maxC - i], maxR - i) : 0;
+
+                // attempt to expand it
+                while (i + 1 + dp[i] < swithpounds.Length && i - 1 - dp[i] >= 0 && swithpounds[i + 1 + dp[i]] == swithpounds[i - 1 - dp[i]])
+                {
+                    dp[i]++;
+                }
+
+                if (dp[i] + i > maxR)
+                {
+                    maxR = dp[i] + i;
+                    maxC = i;
+                }
+
+                if (dp[i] > reslen)
+                {
+                    reslen = dp[i];
+                    res = s.Substring((i - dp[i]) / 2, reslen);
+                }
+            }
+
+            return res;
+        }
+
+        private string preprocess(string s)
+        {
+            var stringbuilder = new StringBuilder();
+            foreach (char ch in s)
+            {
+                stringbuilder.Append("#");
+                stringbuilder.Append(ch);
+            }
+
+            stringbuilder.Append("#");
+
+            return stringbuilder.ToString();
         }
     }
